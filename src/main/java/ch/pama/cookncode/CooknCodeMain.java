b3191387965dea8544ffdca48739d393c8d35eed
Marius Reusch <marius.reusch@googlemail.com>
2017-02-02 22:56:36 +0100

    initial commit

---
+++ src/main/java/ch/pama/cookncode/CooknCodeMain.java
@@ -0,0 +1,12 @@
+package ch.pama.cookncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooknCodeMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooknCodeMain.class, args);
+    }
+}
