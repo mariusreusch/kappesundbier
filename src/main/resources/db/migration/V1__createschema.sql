6c3b61348f5f3ba02b594c02364dfa2900242439
Marius Reusch <marius.reusch@googlemail.com>
2017-02-05 20:26:24 +0100

    change aws profile

--- src/main/resources/db/migration/V1__createschema.sql
+++ src/main/resources/db/migration/V1__createschema.sql
@@ -1,10 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS cookncode
-  DEFAULT CHARACTER SET latin1;
-USE cookncode;
-
--- -----------------------------------------------------
--- Table cookncode.recipe
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS recipe (
   id   BIGINT(20)   NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NULL     DEFAULT NULL,
