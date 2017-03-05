6a283691954d15821036f4c0b9334c060c0a9ccf
marius <marius.reusch@gmail.com>
2017-03-05 17:35:40 +0100

    introduce routing and add dummy page for recipe overview

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,21 +3,36 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
+import { RouterModule, Routes } from '@angular/router';
 import { NoteListComponent } from "./note-list/note-list.component";
 import { NoteAddComponent } from "./note-add/note-add.component";
 import { MaterialModule } from "@angular/material";
+import { HomeComponent } from "./home/home.component";
 import "hammerjs";
-import "style-loader!./styles.scss";
+import "style-loader!./app.module.scss";
+import { RecipeOverviewComponent } from "./recipe-overview/recipe-overview.component";
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'recipe-overview',
+        component: RecipeOverviewComponent
+    }
+];
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
+        RouterModule.forRoot(appRoutes),
         MaterialModule
     ],
     declarations: [
-        AppComponent, NoteListComponent, NoteAddComponent
+        AppComponent, NoteListComponent, NoteAddComponent, HomeComponent, RecipeOverviewComponent
     ],
     bootstrap: [AppComponent]
 })
---
+++ frontend/src/app/recipe-overview/recipe-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'recipe-overview',
+    templateUrl: './recipe-overview.component.html',
+    styleUrls: ['./recipe-overview.component.scss']
+})
+export class RecipeOverviewComponent {
+
+
+}
