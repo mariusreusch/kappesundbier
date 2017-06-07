9aa512b372c2c3f47a4963a6fb0e6b39acbc1521
Marius Reusch <marius.reusch@googlemail.com>
2017-06-07 22:08:57 +0200

    first try with google login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'cookncode-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.login().subscribe();
+  }
+
+  authenticated = false;
+
+  constructor(private http: Http) {
+  }
+
+  login() {
+    return this.http.get("./api/user")
+      .map(response => {
+        this.authenticated = true;
+        return response.json();
+      })
+      .catch(() => {
+        this.authenticated = false;
+        return Observable.of("");
+      });
+  }
+
+  logout() {
+    this.http.post('./logout', {})
+      .map(response => {
+        this.authenticated = false;
+      })
+      .catch(() => {
+        this.authenticated = false;
+        return Observable.of("");
+      }).subscribe();
+
+  }
+
 
 }
