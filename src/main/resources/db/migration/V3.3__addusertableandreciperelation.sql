a880e96e6c084bdd4a3238683d0e9c62a70edea4
Marius Reusch <marius.reusch@googlemail.com>
2017-06-08 19:31:11 +0200

    try to fix flyway

--- src/main/resources/db/migration/V3.3__addusertableandreciperelation.sql
+++ src/main/resources/db/migration/V3.3__addusertableandreciperelation.sql
@@ -1,19 +1,22 @@
 create table user
 (
-	id bigint auto_increment
+	id bigint(20) auto_increment
 		primary key,
 	provider_id varchar(255) null
 );
 
 create table user_recipes
 (
-	user_id bigint not null,
-	recipes_id bigint not null,
+	user_id bigint(20) not null,
+	recipes_id bigint(20) not null,
 	constraint UK_doq4bb6qvl5xf48cpiw3gt1d
 		unique (recipes_id),
 	constraint FKpco0pexrpl6x78mtuukwb49g1
-		foreign key (user_id) references cookncode.user (id),
+		foreign key (user_id) references user (id),
 	constraint FK3fadqc9bwyyhroumcbecbqwpj
-		foreign key (recipes_id) references cookncode.recipe (id)
+		foreign key (recipes_id) references recipe (id)
 ) ;
+
+create index FKpco0pexrpl6x78mtuukwb49g1
+	on user_recipes (user_id) ;
 
