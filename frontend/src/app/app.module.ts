eeda7386e59ea87c6b7a735b6bf5484d2a3ee887
Marius Reusch <marius.reusch1@swisscom.com>
2022-03-29 15:47:41 +0200

    remove unused modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppComponent} from './app.component';
-import {AppRouterModule} from './app-router.module';
-import {KubMaterialModule} from './common/material/kub-material.module';
-import {YesNoDialogComponent} from './common/components/yes-no-dialog/yes-no-dialog.component';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {WeekPlannerModule} from './week-planner/week-planner.module';
-import {AuthenticationModule} from './authentication/authentication.module';
-import {RecipeManagementModule} from './recipe-management/recipe-management.module';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {CookbookManagementModule} from './cookbook-management/cookbook-management.module';
-import {RecipeOverviewModule} from './recipe-overview/recipe-overview.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-router.module';
+import { KubMaterialModule } from './common/material/kub-material.module';
+import { YesNoDialogComponent } from './common/components/yes-no-dialog/yes-no-dialog.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { RecipeManagementModule } from './recipe-management/recipe-management.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { RecipeOverviewModule } from './recipe-overview/recipe-overview.module';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
@@ -34,9 +32,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     KubMaterialModule,
     AuthenticationModule,
     RecipeOverviewModule,
-    RecipeManagementModule,
-    WeekPlannerModule,
-    CookbookManagementModule
+    RecipeManagementModule
   ],
   entryComponents: [
     YesNoDialogComponent
