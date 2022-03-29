011747a91b439c0fa9da7f846aabc5d53d0cabab
Marius Reusch <marius.reusch1@swisscom.com>
2022-03-29 15:18:12 +0200

    angular upgrade step 4

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
 import {AppRouterModule} from './app-router.module';
-import 'hammerjs';
 import {KubMaterialModule} from './common/material/kub-material.module';
 import {YesNoDialogComponent} from './common/components/yes-no-dialog/yes-no-dialog.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
